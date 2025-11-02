67636d71644afd24900bdee74b184e9db34f9481
SantiDrelewicz <santidrelewicz2000@gmail.com>
2025-11-02 02:01:00 -0300

    Refactor dijkstra function to include cost parameter and update neighbor structure

--- Talleres/Taller 3 - Grafos/Problema B - Viaje Intergaláctico/viajeintegalactico.py
+++ Talleres/Taller 3 - Grafos/Problema B - Viaje Intergaláctico/viajeintegalactico.py
@@ -3,29 +3,36 @@ from heapq import heappop as extraer_min, heappush as insertar
 
 INF = float('inf')
 
-def dijkstra(vecinos, s):
+def dijkstra(vecinos, costo, s):
     dist = [INF] * len(vecinos)
     dist[s] = 0
     cp: ColaPrioridad = [(0, s)]
     while len(cp) > 0: 
         dist_actual, u = extraer_min(cp)
         if not dist_actual > dist[u]:
-            for v, p in vecinos[u]:
-                if dist[v] > dist_actual + p:
-                    dist[v] = dist_actual + p
+            for v in vecinos[u]:
+                if dist[v] > dist_actual + costo[(u, v)]:
+                    dist[v] = dist_actual + costo[(u, v)]
                     insertar(cp, (dist[v], v))
     return dist
 
 def main():
     vecinos = [
-        [(1, 4), (5, 3)],
-        [(2, 3), (4, 1)],
-        [(3, 1), (4, 1)],
+        [1, 2, 5],
+        [2, 4],
+        [3, 4],
         [],
-        [(3, 4)],
-        [(4, 3)]
+        [3],
+        [4]
     ]
-    print(dijkstra(vecinos, 0))
+    costo = {
+        (0, 1): 4, (0, 2): 7, (0, 5): 3,
+        (1, 2): 3, (1, 4): 1,
+        (2, 3): 1, (2, 4): 1,
+        (4, 3): 4,
+        (5, 4): 3
+    }
+    print(dijkstra(vecinos, costo, 0))
 
 if __name__ == "__main__":
     main()
